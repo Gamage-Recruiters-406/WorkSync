7f6e353c83866b99361a3543251dd5612cb15f82
AshanDilakshana <167285793+AshanDilakshana@users.noreply.github.com>
2025-12-09 15:01:03 +0530

    create announcement routes

---
+++ Backend/routes/announcement_route.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  createAnnouncement,
+  getAnnouncements,
+  getAnnouncementbyID,
+  getActiveAnnouncements,
+  updateAnnouncement,
+  deleteAnnouncement,
+} from "../Controller/announcementController.js";
+
+const AnnouncementRouter = express.Router();
+
+AnnouncementRouter.post("/createAnnouncement", createAnnouncement);
+AnnouncementRouter.get("/getAnnouncements", getAnnouncements);
+AnnouncementRouter.get("/getAnnouncement/:id", getAnnouncementbyID);
+AnnouncementRouter.get("/getActiveAnnouncements", getActiveAnnouncements);
+AnnouncementRouter.put("/updateAnnouncement/:id", updateAnnouncement);
+AnnouncementRouter.delete("/deleteAnnouncement/:id", deleteAnnouncement);
+
+export default AnnouncementRouter;
