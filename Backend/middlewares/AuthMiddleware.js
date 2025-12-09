947fbc7cf89748913a33589b1a049509d781c670
Kavin <kavinmadusanka20011@gmail.com>
2025-12-09 12:57:31 +0530

    implement required sign in middleware part

---
+++ Backend/middlewares/AuthMiddleware.js
@@ -0,0 +1,15 @@
+import JWT from "jsonwebtoken";
+
+//protected routes using token
+export const requiredSignIn = async(req, res, next) => {
+    try {
+        const decode = JWT.verify(
+            req.headers.authorization,
+            process.env.JWT_SECRET
+        );
+        req.user = decode;
+        next();
+    } catch (error) {
+        console.log(error)
+    }
+}
