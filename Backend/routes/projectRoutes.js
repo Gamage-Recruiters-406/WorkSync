73b0ea732165d0465ee1ec0d97e1a93b5bc9c0f2
Navishka Gunawardana <128272519+navishkaG@users.noreply.github.com>
2025-12-11 18:34:17 +0530

    update createProject to support file attachments

--- Backend/routes/projectRoutes.js
+++ Backend/routes/projectRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { createProjectController } from '../controllers/projectController.js';
 import { requiredSignIn, isManagerOrAdmin } from '../middlewares/AuthMiddleware.js';
+import { uploadProjectFiles } from '../middlewares/uploadMiddleware.js';
 
 // router object
 const router = express.Router();
 
 // CREATE PROJECT -> POST /api/v1/projects/createProject
-router.post("/createProject", requiredSignIn, isManagerOrAdmin, createProjectController);
+router.post("/createProject", requiredSignIn, isManagerOrAdmin, uploadProjectFiles.array("attachments", 5), createProjectController);
 
 export default router;
