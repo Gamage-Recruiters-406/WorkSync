65acdf52ce0114af91d3b15178c8f94b4f23dda8
DamikaDeshan <damikadeshan40@gmail.com>
2025-12-16 15:27:16 +0530

    Create the admin attendance page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,15 @@
-// import Sidebar from './components/Sidebar';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AdminAttendance from './pages/AdminAttendance/AdminAttendance';
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#E5E7EB] flex gap-6 px-6 py-8">
-      <Sidebar role="admin" activeItem="system-settings" />
-      <Sidebar role="employee" activeItem="dashboard" />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/admin/attendance" element={<AdminAttendance />} />
+      </Routes>
+    </Router>
   );
 }
 
