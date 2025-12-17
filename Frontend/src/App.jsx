2f3aae61c710a7fb2a81279a85bae676532ddd1a
DamikaDeshan <damikadeshan40@gmail.com>
2025-12-17 17:09:06 +0530

    create user attendance page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminAttendance from './pages/AdminAttendance';
+import UserAttendance from './pages/UserAttendance';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/admin/attendance" element={<AdminAttendance />} />
+        <Route path="/user/attendance" element={<UserAttendance />} />
       </Routes>
     </Router>
   );
